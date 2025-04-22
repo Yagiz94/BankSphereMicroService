2ad3c5a08b1ce086f70bd13fac917cc29fcbd66b
Yagiz94 <yagizgani94@gmail.com>
2025-04-22 12:45:53 +0300

    It adds required classes to account & transaction modules for Kafka communication.

---
+++ account/src/main/java/com/example/account/enums/TRANSACTION_TYPE.java
@@ -0,0 +1,24 @@
+package com.example.account.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum TRANSACTION_TYPE {
+    DEPOSIT, WITHDRAWAL;
+
+    @JsonCreator
+    public static TRANSACTION_TYPE from(String value) {
+        return TRANSACTION_TYPE.valueOf(value.toUpperCase());
+    }
+
+    // Optional: serialize using name()
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+}
+
+
+
+
+
