9dfc3f4430bc16e296d2d1d725f2ca529e810b69
Yagiz94 <yagizgani94@gmail.com>
2025-05-27 18:48:51 +0300

    It adds redis connection check method for api-gateway and user modules.

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,16 +1,22 @@
 package com.example.apigateway;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
-        }
-)
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
+
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner pingRedis(RedisConnectionFactory f) {
+        return args -> System.out.println("→ [REDIS PING] " + f.getConnection().ping());
     }
 
 }
--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -1,12 +1,20 @@
 package com.example.user;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner pingRedis(RedisConnectionFactory f) {
+        return args -> System.out.println("→ [REDIS PING] " + f.getConnection().ping());
     }
 }
