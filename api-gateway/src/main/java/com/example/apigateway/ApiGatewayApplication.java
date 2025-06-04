87fbe167a832a828b42bfbf799528f5056dc8863
Yagiz94 <yagizgani94@gmail.com>
2025-06-04 12:25:17 +0300

    It removes redundant code lines.

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
 package com.example.apigateway;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.apache.logging.log4j.LogManager; // Add this import
-import org.apache.logging.log4j.Logger;     // Add this import
 
 @SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 public class ApiGatewayApplication {
@@ -17,10 +17,6 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-        // Log a message using the logger after the application has started
-        logger.info("ApiGatewayApplication started successfully!");
-        logger.debug("This is a debug message - should only show if level is DEBUG or lower.");
-        logger.error("An example error message from ApiGatewayApplication.");
     }
 
     @Bean
